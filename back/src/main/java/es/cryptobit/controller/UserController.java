e4903350399ff03973993baf1a3703c946b65d88
calosfernandezbou <carlosfb2005@gmail.com>
2026-02-11 19:31:27 +0100

    getUsers y arreglar properties

--- back/src/main/java/es/cryptobit/controller/UserController.java
+++ back/src/main/java/es/cryptobit/controller/UserController.java
@@ -6,9 +6,12 @@ import org.bson.json.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,11 +19,19 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    // http://localhost:8080/API/NewUser
     @PostMapping("/API/NewUser")
     public ResponseEntity<Object> addNewUser(@RequestBody User newUser) {
 
         userRepository.save(newUser);
         System.out.println("Usuario insertado: " + newUser.getFirstName());
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    // http://localhost:8080/API/SeeUsers
+    @GetMapping("/API/SeeUsers")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return ResponseEntity.ok(users);
     }
 }
