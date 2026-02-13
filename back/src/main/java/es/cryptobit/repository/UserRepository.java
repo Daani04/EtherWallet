ae7887d6322d8bc0bb3883ec6e6df98ce90462ee
calosfernandezbou <carlosfb2005@gmail.com>
2026-02-13 16:09:21 +0100

    añadir intento de login

--- back/src/main/java/es/cryptobit/repository/UserRepository.java
+++ back/src/main/java/es/cryptobit/repository/UserRepository.java
@@ -2,6 +2,8 @@ package es.cryptobit.repository;
 
 import es.cryptobit.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
