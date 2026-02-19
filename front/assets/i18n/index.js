dbffcf6fa53cbc70d7afaf691adb2ec8bb9e5ead
dani <danielarastell302004@gmail.com>
2026-02-19 18:41:37 +0100

    cambios

--- front/assets/i18n/index.js
+++ front/assets/i18n/index.js
@@ -19,15 +19,4 @@ i18n.use(initReactI18next).init({
   },
 });
 
-i18n
-    .use(initReactI18next)
-    .init({
-        resources,
-        lng: 'EN',
-        fallbackLng: 'ES',
-        interpolation: {
-            escapeValue: false,
-        },
-    });
-
 export default i18n;
